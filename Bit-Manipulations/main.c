3148d42048694de46b7374872eebbfcaec326ea2
Ajay-Embed <ajaycrajan@gmail.com>
2020-10-26 00:19:03 +0530

    added bit string search

--- Bit-Manipulations/main.c
+++ Bit-Manipulations/main.c
@@ -1,24 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*
-In this program we will find duplicates in a given string using bit-wise operations
 
-*/
-
-//Core logic implementation
-void check_duplicate(char *str)
+int main(void)
 {
 
-}
-
-
-//Application
-int main()
-{
-    char s[100];
-    scanf("%[^\n]s", s);
-
-    check_duplicate(s);
-
-    return 0;
+return 0;
 }

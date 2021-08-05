4e36b26b9a6435b1ab6ce67fddfe1e03b3ad4851
pran005 <16bec020@nirmauni.ac.in>
2021-08-05 17:48:18 +0100

    Minor corrections

--- Fundamentals-of-OOP-using-C++/3_operator_overloading.cpp
+++ Fundamentals-of-OOP-using-C++/3_operator_overloading.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+/**************************************************************
+ * Operator overloading is an example of Polymorphism
+ * i.e. the same operator acts differently depending on
+ * situations. The best example of this in C++ is the 
+ * operator " << ", with std::cout it behaves differently
+ * and with numbers (int a = 1<<2) it behaves as a binary
+ * left shift operator. The reason is that it's overloaded.
+ *
+ * The following piece of code demonstrates the way one can
+ * overload operators. It overloads the "+" arithmetic operator 
+ * to add two variables of the complex class (2 complex numbers)
+ *
+ ***************************************************************/
+
 class Complex
 {
     public: 

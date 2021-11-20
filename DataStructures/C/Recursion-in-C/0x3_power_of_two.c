a4857af3634dca7db40c3b7a07252016b7ec07ee
Ajay-Embed <ajaycrajan@gmail.com>
2021-11-20 18:22:58 +0530

    2 power using recursion

---
+++ DataStructures/C/Recursion-in-C/0x3_power_of_two.c
@@ -0,0 +1,28 @@
+/*
+    power of two using recursion
+*/
+#include <stdio.h>
+
+int power_of_two(int x, int exp)
+{
+    if(exp == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (power_of_two(x, exp-1)*x);
+    }
+}
+
+
+int main(void)
+{
+    int x = 5; 
+
+
+    int res = power_of_two(x, 3);
+    printf("%d", res);
+
+    return 0;
+}
